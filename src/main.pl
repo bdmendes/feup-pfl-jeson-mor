895932064e902f2da220d95b9bc408bf4f2e9685
Bruno Mendes <brod56@sapo.pt>
2022-01-06 10:30:06 +0000

    Check if entered move is a player piece

--- src/main.pl
+++ src/main.pl
@@ -1,1 +1,11 @@
-play().
+:-ensure_loaded('game_logic.pl').
+:-ensure_loaded('view.pl').
+
+play:-
+    initial_state(5,b,GameState),
+    repeat,
+    display_game(GameState),
+    read(X),
+    atom_chars(X, Move),
+    (move(GameState, Move, A) -> write('Valid move!\n'); write('Invalid move!\n')),
+    fail.
--- src/utils.pl
+++ src/utils.pl
@@ -8,3 +8,14 @@ replicate_aux(0,_,List,List).
 replicate_aux(Size, Elem, List, Acc):-
     NSize is Size - 1,
     replicate_aux(NSize, Elem, List, [Elem|Acc]).
+
+% digits_value(+List, -Value)
+digits_value(String, Value) :-
+    digits_value_aux(String,0,Value).
+digits_value_aux([], Value, Value).
+digits_value_aux([H|T], Acc, Value):-
+    char_code('0', ZeroCharCode),
+    char_code(H, HeadCharCode),
+    CharCodeDiff is HeadCharCode - ZeroCharCode,
+    Nacc is 10*Acc + CharCodeDiff,
+    digits_value_aux(T, Nacc, Value).
