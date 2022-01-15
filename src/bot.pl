c17a5af3d7c81fbc307d2705c10057a5c636fad9
up201907216 <up201907216@edu.fe.up.pt>
2022-01-15 23:09:45 +0000

    DFS based path search - Takes too long

--- src/bot.pl
+++ src/bot.pl
@@ -7,17 +7,22 @@ findall(_, nth0_nested(_, _, B, P), L),
     length(L, M).
 
 % knight_hops(?StartPos, ?EndPos, ?Hops)
-knight_hops_needed(SP, EP, H):-
-    knight_hops_needed_aux(SP, EP, 0/0, H, 0).
+knight_hops_needed([SP , EP, H):-
+    knight_hops_needed_aux([SP], EP, L, []),
+    length(L, H).
 
-knight_hops_needed_aux(CP, CP, _,  Acc, Acc).
-knight_hops_needed_aux(CC/CR, EP, LC/LR, H, Acc):-
+% DFS
+knight_hops_needed_aux(EP, EP, L, L).
+knight_hops_needed_aux(CP, EP, L, V):-
+    \+member(CP, V), 
+    CC-CR = CP,
     CC > 0,
     CC =< 5,
     CR > 0,
-    CR =< 5, 
-    knight_move(CC-CR-EC-ER),
-    EC \= LC,
-    ER \= LR,
-    NAcc is Acc + 1,
-    knight_hops_needed_aux(EC/ER, EP, CC/CR, H, NAcc).
+    CR =< 5,
+    knight_move(CC-CR-NC-NR),
+    knight_hops_needed_aux(NC-NR, EP, L, [CP|V]).
+
+
+
+    
