f319879c8d514dd5c806b4a07d8ce702045bf981
Bruno Mendes <brod56@sapo.pt>
2022-01-18 15:33:38 +0000

    Cleanup bfs code

--- src/bot.pl
+++ src/bot.pl
@@ -1,19 +1,25 @@
 :-ensure_loaded('utils.pl').
 :-ensure_loaded('logic.pl').
 
-% material_count(+Player, +Board, -MaterialNr)
+% material_count(+Player, +Board, -Material)
 material_count(P, B, M):-
-findall(_, nth0_nested(_, _, B, P), L),
+    findall(_, nth0_nested(_, _, B, P), L),
     length(L, M).
 
-% knight_hops(?StartPos, ?EndPos, ?Hops)
-knight_hops_needed(CB,SC-SR, EP, H):-
-    find_path(CB, [[SC-SR]], EP, M),
-    length(M, N),
-    H is N-1.
+% knight_hops(+Board, ?StartPosition, ?EndPosition, ?Hops)
+knight_hops(B, SC-SR, EC-ER, H):-
+    path(B, [[SC-SR]], EC-ER, P),
+    length(P, S),
+    H is S-1.
 
-find_path(_, [[EP|T]|_], EP, [EP|T]).
-find_path(CB, [[CC-CR|T]|LM], EP, M):-
-    findall([NC-NR, CC-CR|T], (can_move([_,CB,_], CC-CR-NC-NR), \+member(NC-NR, [CC-CR|T])), NM1),
-    append(LM, NM1, NM2),
-    find_path(CB, NM2, EP, M).
+% path(+Board, ?StartPosition, ?EndPosition, ?Path)
+path(B, SC-SR, EC-ER, P):-
+    bfs_path(B, SC-SR, [[EC-ER]], P).
+
+% bfs_path(+Board, +EndPosition, +Queue, ?Path)
+bfs_path(_, EC-ER, [[EC-ER|T]|_], [EC-ER|T]):- !.
+bfs_path(B, EC-ER, [V|Q], P):-
+    V = [CC-CR|T],
+    findall([NC-NR, CC-CR|T], (can_move([_,B,_], CC-CR-NC-NR), \+member(NC-NR, [CC-CR|T])), L),
+    append(Q, L, NQ),
+    bfs_path(B, EC-ER, NQ, P).
